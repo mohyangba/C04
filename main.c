65b6294a145f80bb736809d8c3d078b4e09d127e
mohyangba <mohyangba@users.noreply.github.com>
2023-10-08 23:10:17 +0900

    04

--- main.c
+++ main.c
@@ -5,10 +5,24 @@ int main(int argc, char *argv[])
 {
     int a;
     
-    printf("input seconds : ");
+    //1. 4로 나누어 떨어져야 함
+    
+    //2. 100으로 나누어 떨어지지 않아야 함
+    
+    //3. 위를 만족하지 않아도 400으로 나누어 떨어짐
+    
+    printf("input a year : "); 
     scanf("%i", &a);
     
-    printf("%i seconds - %i : %i \n", a, a/60, a%60);
+    if ( (a%4==0 && a%100!=0) || (a%400==0) )
+    {
+         printf("%i is leap year.\n", a);
+    }
+    else
+    {
+
+        printf("%i is not leap year.\n", a);
+    }
     
   system("PAUSE");
   return 0;
