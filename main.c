40f3446ee4169b8adedd9fe65a896c59e6ba2b58
mohyangba <mohyangba@users.noreply.github.com>
2023-10-08 23:00:20 +0900

    03

--- main.c
+++ main.c
@@ -3,33 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-  int num1, num2;
-  int x, y, z, m;
-  float k;
-  
-  printf("Enter the first integers: ");
-  scanf("%d", &num1);
-  
-  printf("Enter the second integers: ");
-  scanf("%d", &num2);
-  
-  if (num2 == 0){
-        printf("Error: You can't divide a number with 0");
-        }
-        else {
-             x = num1 + num2;
-             y = num1 - num2;
-             z = num1*num2;
-             k = (float)num1/num2;
-             m = num1 % num2;
-             }
-             
-  printf("+ result is %d\n", x);
-  printf("- result is %d\n", y);
-  printf("* result is %d\n", z);
-  printf("/ result is %.2f\n", k);
-  printf("%% result is %d\n", m);
-  
+    int a;
+    
+    printf("input seconds : ");
+    scanf("%i", &a);
+    
+    printf("%i seconds - %i : %i \n", a, a/60, a%60);
+    
   system("PAUSE");
   return 0;
 }
